21ba2cd0c719f2d5a0ce2c6aa7fb3b6e52d0b2c0
Himanshu <himanshuvishwakarma383@gmail.com>
2023-10-07 08:41:55 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const form=document.querySelector('form');
+form.addEventListener('submit',(e)=>{
+    e.preventDefault()
+ const height= parseInt(document.querySelector('#height').value);
+ const weight= parseInt(document.querySelector('#weight').value);
+ const result=document.querySelector('#result');
+ if(height==='' || height<0 || isNaN(height)){
+    result.innerHTML=`please give valid height ${height}`
+ }
+ else if(weight==='' || weight<0 || isNaN(weight)){
+    result.innerHTML=`please give valid weight ${weight}`
+ }else{
+   const bmi= (weight/((height*height)/10000)).toFixed(2)
+   
+   result.innerHTML=`<span>${bmi}</span>`
+   const show=document.querySelector('#show')
+   if(bmi<18.6){
+    show.innerHTML=`you are under weight ${weight}`
+
+   }
+   if(bmi>18.6 && bmi<24.9){
+    show.innerHTML=`this is normal${weight}`
+   }
+   if(bmi>24.9){
+    show.innerHTML=`thsi is overWeight`
+   }
+ }
+})
